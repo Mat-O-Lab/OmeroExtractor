d6d902acca51c7c0e0a113a9116d01265917de89
alexanderreinholdt <93722850+alexanderreinholdt@users.noreply.github.com>
2022-03-23 14:21:39 +0100

    Update main.py
    Implemented change proposed by Michael Hoffmann.

--- main.py
+++ main.py
@@ -91,33 +91,21 @@ assert return_value.status_code == 200
 assert response["success"]
 
 #
-# Get the list of projects we have access to
+# Get the URL the images are stored under
 #
-projects_url = all_urls["url:projects"]
-projects = session.get(projects_url).json()
+images_url = all_urls["url:images"]
+images = session.get(images_url).json()
 
 #
-# Now get all available images in the projects and write
-# their metadata into files (one for each image)
+# Loop through the available images and save the
+# JSON data to one file each
 #
-for p in projects["data"]:
-    # Get the URL for the datasets of the project
-    project_url = projects_url + str(p["@id"]) + "/"
-    project = session.get(project_url).json()
-    project_datasets_url = project["data"]["url:datasets"]
-    # Get the datasets from the respective project
-    project_datasets = session.get(project_datasets_url).json()
-    # Get the images from the datasets
-    for d in project_datasets["data"]:
-        project_images_url = d["url:images"]
-        project_images = session.get(project_images_url).json()
-        # Write the metadata of each image into a json file
-        for i in project_images["data"]:
-            image_url = i["url:image"]
-            json_data = session.get(image_url).json()
-            json_data.update({"ImageUrl": image_url})
-            f = open(json_data["data"]["Name"]+".json", "w")
-            f.write(json.dumps(json_data, indent=4, sort_keys=True))
-            f.close()
+for i in images["data"]:
+    image_url = i["url:image"]
+    json_data = session.get(image_url).json()
+    json_data.update({"ImageUrl": image_url})
+    f = open(json_data["data"]["Name"]+".json", "w")
+    f.write(json.dumps(json_data, indent=4, sort_keys=True))
+    f.close()
 
 print("All metadata saved. Goodbye.")
